c7365d530966cb04b4da8a6bba53af9b57a1ab8d
Marcincin9 <marcino@gmx.com>
2022-09-02 20:17:45 +0100

    add props for dynamic use

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <Todo />
-      <Todo />
-      <Todo />
+      <Todo text="Learn React" />
+      <Todo text="Eat a Meal" />
+      <Todo text="Go to sleep" />
+      <Todo text="Learn React" />
     </div>
   );
 }
